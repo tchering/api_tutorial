faa7c8b86ccd355397171023a9e8d531b67a17c9
tchering <sonam.sherpa.lama@gmail.com>
2024-10-12 18:53:14 +0200

    added bootstrap styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,11 @@ class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new(ENV["FLICKR_API_KEY"], ENV["FLICKR_SECRET"])
     user_id = params[:user_id]
-    @photos = flickr.photos.search(user_id: user_id) if params[:user_id]
+    begin
+      @photos = flickr.photos.search(user_id: user_id) if params[:user_id]
+    rescue Flickr::FailedResponse => e
+      @error = "Failed to fetch photos: #{e.message}"
+      @photos = []
+    end
   end
 end
