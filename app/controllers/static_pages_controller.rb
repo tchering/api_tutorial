033bd517716499f686f7489ff20cb1e9307960ae
tchering <sonam.sherpa.lama@gmail.com>
2024-10-12 22:49:45 +0200

    implemented sortable library to make image dragable

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,15 @@ class StaticPagesController < ApplicationController
     user_id = params[:user_id]
     begin
       @photos = flickr.photos.search(user_id: user_id) if params[:user_id]
+      respond_to do |format|
+        format.html
+        format.xml { render xml: @photos }
+        format.json { render json: @photos }
+      end
     rescue Flickr::FailedResponse => e
       @error = "Failed to fetch photos: #{e.message}"
       @photos = []
     end
   end
 end
+
